a77aefa07f19f1ac054498674c688b623cd874de
Paul Torres <paul.torres@stash.com>
2021-10-08 10:09:56 -0400

    part way through fundamentals 1

---
+++ 01-Fundamentals-Part-1/js_assignments/script.js
@@ -0,0 +1,22 @@
+// value and variables
+let country = 'USA';
+let continent = 'North America';
+let population = 3*(10**8);
+
+console.log('--Value and Variables--');
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log('------------');
+
+// value and variables
+let isIsland = false;
+let language;
+
+console.log('--Data Types--');
+console.log(isIsland);
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(language)
+console.log('------------');
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+let js = 'radical!';
+console.log(1.83*49.25);
+
+console.log("Hello all of you")
+
+let firstname = "Paul";
+console.log(firstname);
+
+
+
+
+
+
